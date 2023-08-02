f7fc72c4fdff2676fd4aec011fe51160c8e6f949
Dmitry Mishin <dmitrymishin@dmitry.local>
2023-08-02 21:32:43 +0200

    replace test with it

--- src/shared/operators/array.test.ts
+++ src/shared/operators/array.test.ts
@@ -1,26 +1,24 @@
 import { array } from './array';
 
-describe('array operator', () => {
-  test('a single value should transform to array', () => {
-    const singleValues = [
-      null,
-      undefined,
-      { hello: 'world' },
-      1,
-      'hello',
-      () => {},
-    ];
+it('should transform any single value to array', () => {
+  const singleValues = [
+    null,
+    undefined,
+    { hello: 'world' },
+    1,
+    'hello',
+    () => {},
+  ];
 
-    singleValues.forEach((value) => {
-      expect(array(value)).toMatchObject([value]);
-    });
+  singleValues.forEach((value) => {
+    expect(array(value)).toMatchObject([value]);
   });
+});
 
-  test('an array should stay the array', () => {
-    expect(array(['hello', 'world'])).toMatchObject(['hello', 'world']);
-  });
+it('should not update the array', () => {
+  expect(array(['hello', 'world'])).toMatchObject(['hello', 'world']);
+});
 
-  test('an empty arguments should transform to empty array', () => {
-    expect(array()).toMatchObject([]);
-  });
+it('should return an empty array', () => {
+  expect(array()).toMatchObject([]);
 });
--- src/shared/operators/capitalizeFirstLetter.test.ts
+++ src/shared/operators/capitalizeFirstLetter.test.ts
@@ -1,27 +1,25 @@
 import { capitalizeFirstLetter } from './capitalizeFirstLetter';
 
-describe('capitalizeFirstLetter operator', () => {
-  test('first letter should be uppercase', () => {
-    expect(capitalizeFirstLetter('hello')).toBe('Hello');
-  });
+it('should capitalize first letter', () => {
+  expect(capitalizeFirstLetter('hello')).toBe('Hello');
+});
 
-  test('empty line should be empty line', () => {
-    expect(capitalizeFirstLetter('')).toBe('');
-  });
+it('should return an empty line', () => {
+  expect(capitalizeFirstLetter('')).toBe('');
+});
 
-  test('handling wrong types', () => {
-    const wrongInputs: any[] = [
-      null,
-      undefined,
-      {},
-      [],
-      () => {},
-      1,
-      Symbol('test'),
-    ];
+it('should handle any wrong type', () => {
+  const wrongInputs: any[] = [
+    null,
+    undefined,
+    {},
+    [],
+    () => {},
+    1,
+    Symbol('test'),
+  ];
 
-    wrongInputs.forEach(
-      (value) => expect(capitalizeFirstLetter(value)).toBe(''),
-    );
-  });
+  wrongInputs.forEach(
+    (value) => expect(capitalizeFirstLetter(value)).toBe(''),
+  );
 });
