98382029ba052aa07c93fccc276606cb2299316e
Dmitry Mishyn <mishyndmitry@gmail.com>
2023-08-24 06:59:14 +0200

    bump deps

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const config = {
   moduleDirectories: ['node_modules', 'src/'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
 };
 
