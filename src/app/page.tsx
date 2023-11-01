e0e90737e2c3f1f201969d4c149ad20830c5f47d
Dmitry Mishyn <mishyndmitry@gmail.com>
2023-11-01 18:35:54 +0100

    add reshape example on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,54 @@
 'use client';
 
-/* eslint-disable max-len */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { useState } from 'react';
-
-import Container from '@/shared/components/Container';
-import Editor from '@/shared/Editor';
-import Paragraph from '@/shared/Paragraph';
-
-let i = 1000;
-const newKey = () => {
-  i += 1;
-  return `${i}`;
-};
-
-let startArr = 0;
+import { reshape } from '@/shared/browser/manipulations';
 
 export default function Home() {
-  const [state, setState] = useState([
-    { brick: 'paragraph', id: newKey(), children: '1Lorem ipsum dolar sit amet' },
-    { brick: 'paragraph', id: newKey(), children: '2hello world' },
-    { brick: 'paragraph', id: newKey(), children: ['3one child', ' ', 'another child'] },
-    {
-      brick: 'paragraph', id: newKey(), children: '4hello world with attributes', attributes: { test: true },
-    },
-    {
-      brick: 'container',
-      children: [
-        { brick: 'paragraph', id: newKey(), children: '1Lorem ipsum dolar sit amet' },
-        { brick: 'paragraph', id: newKey(), children: '2hello world' },
-        { brick: 'paragraph', id: newKey(), children: ['3one child', ' ', 'another child'] },
-      ],
-    },
-  ]);
-
   return (
     // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
     <main
       contentEditable
       suppressContentEditableWarning
+      style={{
+        padding: 20,
+      }}
+      onKeyDown={(event) => {
+        let range;
+        if (event.code === 'Escape') {
+          event.preventDefault();
+          range = reshape(
+            { create: () => document.createElement('strong'), selector: 'strong' },
+            window.getSelection()!.getRangeAt(0)!,
+            document.querySelector('main'),
+          );
+        }
+
+        if (event.code === 'Tab') {
+          event.preventDefault();
+          range = reshape(
+            { create: () => document.createElement('i'), selector: 'i' },
+            window.getSelection()!.getRangeAt(0)!,
+            document.querySelector('main'),
+          );
+        }
+
+        if (range) {
+          window.getSelection()?.removeAllRanges();
+          window.getSelection()?.addRange(range);
+        }
+      }}
     >
-      <button
-        type="button"
-        onClick={() => {
-          const newId = newKey();
-          const newValue = { brick: 'paragraph', children: `${state.length + 1} - ${Math.random()}`, id: newId };
-          startArr += 1;
-          setState((value: any) => [
-            newValue,
-            ...value.slice(0, startArr),
-            { ...value[startArr], children: [`${value[startArr].children[0]} ${value.length + 1} `, ...value[startArr].children.slice(1)] },
-            ...value.slice(startArr + 1),
-          ]);
-        }}
-      >
-        Update
-      </button>
-      <Editor
-        value={state}
-        bricks={[Paragraph, Container]}
-      />
+      It&apos;s not react components yet. You can try to use
+      {' '}
+      <strong>escape (to make text strong)</strong>
+      {' '}
+      and
+      {' '}
+      <em>tab (to make text em)</em>
+      {' '}
+      to see how it works.
+      <br />
+      <br />
+      You can write text with spaces/new lines and experiment with reshape function.
     </main>
   );
 }
