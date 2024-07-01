22d2183b7d3522cd4c76bdac731a7a6a3be2b638
Dmitry Mishyn <mishyndmitry@gmail.com>
2024-07-01 22:47:19 +0200

    get rid of Reader from all the functions

--- packages/editor/src/bricks/bricksToReact.tsx
+++ packages/editor/src/bricks/bricksToReact.tsx
@@ -1,7 +1,6 @@
-import { array } from '@brickifyio/operators';
+import { array, tap } from '@brickifyio/operators';
 import { add, type Node, of } from '@brickifyio/utils/slots-tree';
 import { flow, pipe } from 'fp-ts/lib/function';
-import * as I from 'fp-ts/lib/Identity';
 import * as O from 'fp-ts/lib/Option';
 import * as R from 'fp-ts/lib/Reader';
 import {
@@ -33,20 +32,7 @@ type Options = {
   parent: Node;
 };
 
-const preparePathRef = <T extends { index: number; cached?: CacheItem }>({
-  index,
-  cached,
-}: T): R.Reader<Options, PathRef> =>
-  ({ parentPathRef }) => {
-    const pathRef = cached?.pathRef
-      ?? createRef<() => string[]>() as MutableRefObject<() => string[]>;
-
-    pathRef.current = () => [...parentPathRef.current(), `${index}`];
-
-    return pathRef;
-  };
-
-const safeValue = ({
+export const safeValue = ({
   value,
   ...data
 }: { index: number; value: unknown }): O.Option<{ index: number; value: BrickValue }> => {
@@ -57,98 +43,108 @@ const safeValue = ({
   return O.none;
 };
 
-const getCached = <T extends { value: BrickValue }>(
-  { value }: T,
-): R.Reader<Options, CacheItem | undefined> =>
-  ({ cache }) => cache.get(value);
+export const safeComponent = ({ slots }: Pick<Options, 'slots'>) =>
+  <T extends { value: BrickValue }>(option: O.Option<T>) => pipe(
+    option,
+    O.bind('Component' as Exclude<"Component", keyof T>, flow(
+      ({ value }) => slots[value.brick],
+      O.fromNullable,
+    )),
+  );
 
-const createChange = <T extends { value: BrickValue; pathRef: PathRef }>({
-  pathRef,
-  value,
-}: T): R.Reader<Options, (options: { type: Change['type'] }) => void> => ({ onChange }) =>
-  ({ type, ...newValue }) => {
-    onChange({
-      type,
-      value: newValue
-        ? { ...value, ...newValue }
-        : value,
-      path: pathRef.current(),
-    });
+export const addCached = ({ cache }: Pick<Options, 'cache'>) => <T extends { value: BrickValue }>(
+  data: T,
+) => ({
+  ...data,
+  cached: cache.get(data.value),
+});
+
+const addPathRef = ({ parentPathRef }: Pick<Options, 'parentPathRef'>) =>
+  <T extends { index: number; cached?: CacheItem }>(data: T) => {
+    const pathRef = data.cached?.pathRef
+      ?? createRef<() => string[]>() as MutableRefObject<() => string[]>;
+
+    pathRef.current = () => [...parentPathRef.current(), `${data.index}`];
+
+    return { ...data, pathRef };
   };
 
-const getComponent = <T extends { value: BrickValue }>(
-  { value }: T,
-): R.Reader<Options, ComponentType | undefined> => {
-  return ({ slots }) => slots[value.brick];
+const addChange = ({ onChange }: Pick<Options, 'onChange'>) =>
+  <T extends { value: BrickValue; pathRef: PathRef }>(data: T) => ({
+    ...data,
+    change: ({ type, ...newValue }: { type: Change['type'] }) => onChange({
+      type,
+      value: newValue
+        ? { ...data.value, ...newValue }
+        : data.value,
+      path: data.pathRef.current(),
+    }),
+  });
+
+export const addSlotsMeta = <T extends { Component: ComponentType }>(value: T) => {
+  const slotMap = hasSlots(value.Component) ? value.Component.slots : {};
+  return {
+    ...value,
+    slotMap,
+    slotNames: Object.keys(slotMap),
+  };
 };
+
+export const addTreeNode = <T extends { value: BrickValue; slotNames: string[]; cached?: CacheItem }>(data: T) => ({
+  ...data,
+  node: data.cached?.node ?? of(data.value, data.slotNames),
+});
 
 export const bricksToReact = flow(
   array<unknown>,
   R.traverseArrayWithIndex<Options, unknown, ReactNode | null>(flow(
     (index, value) => R.of({ index, value }),
-    R.map(safeValue),
-    R.chain((option) => (deps: Options) => pipe(
-      option,
-      O.bind('Component', flow(getComponent, I.ap(deps), O.fromNullable)),
-    )),
-    R.chain((option) => (deps: Options) => pipe(
-      option,
-      O.bind('cached', flow(getCached, I.ap(deps), O.some)),
-    )),
-    R.chain((option) => (deps: Options) => pipe(
-      option,
-      O.bind('pathRef', flow(preparePathRef, I.ap(deps), O.some)),
-    )),
-    R.chain((option) => (deps: Options) => pipe(
-      option,
-      O.bind('change', flow(createChange, I.ap(deps), O.some)),
-    )),
-    R.chain((option) => ({ parentPathRef, parent, onChange, cache, slots }) => pipe(
-      option,
-      O.chain(({ Component, cached, value, pathRef, change, index }) => {
-        const slotsMap = hasSlots(Component) ? Component.slots : {};
-        const slotNames = Object.keys(slotsMap);
-        const node = cached?.node ?? of(value, slotNames);
+    R.chain((values) => (deps: Options) => pipe(
+      values,
+      safeValue,
+      safeComponent(deps),
+      O.map(addCached(deps)),
+      O.map(addPathRef(deps)),
+      O.map(addChange(deps)),
+      O.map(addSlotsMeta),
+      O.map(addTreeNode),
+      O.map(tap(({ node }) => add(deps.parent, deps.parentPathRef.current().at(-1)!, node))),
+      O.chain(({ node, change, index, Component, cached, pathRef, value, slotMap }) => pipe(
+        O.fromNullable(cached?.element),
+        O.altW(() => {
+          const { id, brick: _brick, ...rest } = value;
+          const slotProps = Object
+            .entries(slotMap)
+            .reduce<Record<string, readonly ReactNode[]>>((acc, [name, childBricks]) => {
+              const childValue = value[name as keyof typeof value];
+              acc[name] = bricksToReact(childValue)({
+                onChange: deps.onChange,
+                cache: deps.cache,
+                // eslint-disable-next-line -- TODO: Check it
+                slots: (childBricks === 'inherit' ? deps.slots : childBricks) as any || {},
+                parentPathRef: { current: () => [...pathRef.current(), name] },
+                parent: node,
+              });
+              return acc;
+            }, {});
 
-        add(parent, parentPathRef.current().at(-1)!, node);
+          const element = (
+            <Component
+              {...rest}
+              {...slotProps}
+              {...{
+                onChange: change,
+                brick: { value, pathRef },
+              }}
+              key={id || index}
+            />
+          );
 
-        return pipe(
-          O.fromNullable(cached?.element),
-          O.altW(() => {
-            const { id, brick: _brick, ...rest } = value;
-            const slotProps = Object
-              .entries(slotsMap)
-              .reduce<Record<string, readonly ReactNode[]>>((acc, [name, childBricks]) => {
-                const childValue = value[name as keyof typeof value];
-                acc[name] = bricksToReact(childValue)({
-                  onChange,
-                  cache,
-                  // eslint-disable-next-line -- TODO: Check it
-                  slots: (childBricks === 'inherit' ? slots : childBricks) as any || {},
-                  parentPathRef: { current: () => [...pathRef.current(), name] },
-                  parent: node,
-                });
-                return acc;
-              }, {});
+          deps.cache.set(value, { element, node, pathRef });
 
-            const element = (
-              <Component
-                {...rest}
-                {...slotProps}
-                {...{
-                  onChange: change,
-                  brick: { value, pathRef },
-                }}
-                key={id || index}
-              />
-            );
-
-            cache.set(value, { element, node, pathRef });
-
-            return O.some(element);
-          }),
-        );
-      }),
+          return O.some(element);
+        }),
+      )),
     )),
     R.map(O.getOrElseW(() => null)),
   )),
