2e3834d45a16cf94bce200ffa04333236078cd14
Dmitry Mishyn <mishyndmitry@gmail.com>
2024-08-08 23:46:08 +0200

    get rid of test components

--- apps/editor-example/src/app/page.tsx
+++ apps/editor-example/src/app/page.tsx
@@ -6,11 +6,9 @@
 
 import { extend, withProps, withSlots } from '@brickifyio/core';
 import { ShiftEnterBr } from '@brickifyio/editor/components/Br';
-import Container from '@brickifyio/editor/components/Container';
 import Em from '@brickifyio/editor/components/Em';
 import Heading from '@brickifyio/editor/components/Heading';
 import List from '@brickifyio/editor/components/List';
-import Profile from '@brickifyio/editor/components/Profile';
 import Strong from '@brickifyio/editor/components/Strong';
 import Table from '@brickifyio/editor/components/Table';
 import Editor from '@brickifyio/editor/Editor';
@@ -25,9 +23,6 @@ const Page = () => {
       return `${i}`;
     };
   }, []);
-  // const [state2, setState2] = useState<unknown>(() => [
-  //   { brick: 'Paragraph', id: newKey(), value: '1Lorem <strong style="color: red"><strong>i<em>ps</em>um</strong></strong> dolar sit <strong>amet</strong>' },
-  // ]);
   const [state, setState] = useState<unknown>(() => Array.from({ length: 1 }, () => [
     {
       id: newKey(),
@@ -55,53 +50,24 @@ const Page = () => {
       ],
     },
     { brick: 'Paragraph', id: newKey(), value: 'It is a <strong>paragraph</strong>' },
-    {
-      brick: 'Profile',
-      id: newKey(),
-      children: 'Hi <strong>everyone</strong>!',
-      visible: true,
-      // children: [
-      //   { brick: 'Paragraph', id: newKey(), children: 'Hi <strong>everyone</strong>!' },
-      // ],
-    },
     // { brick: 'Paragraph', id: newKey(), value: '1Lorem ipsum dolar sit amet' },
     // ...Array.from({ length: 2000 }, () => ({
     //   brick: 'Paragraph', id: newKey(), value: `${newKey()} hello world`,
     // })),
   ]).flat());
-  const [editable, setEditable] = useState(true);
 
   return (
     <div>
-      <button
-        type="button"
-        onClick={() => setEditable((oldEditable) => !oldEditable)}
-      >
-        {editable ? 'Disable' : 'Enable'}
-        {' '}
-        editable mode
-      </button>
       <Editor
         style={{ padding: 20 }}
         // eslint-disable-next-line -- TODO: check it
         value={state as any}
-        editable={editable}
+        editable
         onChange={(newValue) => { setState(newValue); }}
         bricks={[
           List,
-          extend(
-            Editor,
-            withProps({
-              bricks: [
-                extend(Paragraph, withProps({ component: 'div', bricks: [ShiftEnterBr] })),
-                Container,
-              ] ,
-            }),
-          ),
           Heading,
-          extend(Paragraph, withProps({ component: 'article', bricks: [Em, Strong, ShiftEnterBr] })),
-          extend(Container, withSlots({ children: 'inherit' })),
-          Profile,
+          extend(Paragraph, withProps({ bricks: [Em, Strong, ShiftEnterBr] })),
           Table,
         ]}
       />
