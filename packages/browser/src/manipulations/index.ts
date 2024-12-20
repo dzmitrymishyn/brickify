5a039df9fc6b4ecb845344b307b2732560da7ea5
Dmitry Mishyn <mishyndmitry@gmail.com>
2024-12-21 00:03:35 +0100

    clean up the manipulations exports

--- packages/browser/src/manipulations/index.ts
+++ packages/browser/src/manipulations/index.ts
@@ -1,7 +1,3 @@
-export * from './clearNodes';
-export * from './clearSiblings';
 export * from './expose';
-export * from './prepareRange';
 export * from './reshape';
 export * from './surround';
-export * from './wrapToNode';
